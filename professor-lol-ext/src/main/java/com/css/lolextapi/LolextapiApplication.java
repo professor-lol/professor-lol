c9d2d7484ecf5974d304b67ba64055cf22d38d5f
박찬인 <chan@bagchan-in-ui-MacBookPro.local>
2019-07-31 22:16:20 +0900

    init commit ext api module

---
+++ professor-lol-ext/src/main/java/com/css/lolextapi/LolextapiApplication.java
@@ -0,0 +1,13 @@
+package com.css.lolextapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolextapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LolextapiApplication.class, args);
+	}
+
+}
