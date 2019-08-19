6372ddc88815a449d10fb0d104a69173291c55b3
csbsjy <seojaeyeon@lotte.net>
2019-08-20 01:44:52 +0900

    professor-lol-21 구글 로그인 및 추가 회원가입 페이지 구현

---
+++ professorlol-web/src/front/src/views/RegistrationForm.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <label>ID</label><input name="id" placeholder="lol 계정 id" type="text" v-model="lolId">
+        <input type="button" v-on:click="formSubmit" value="회원가입">
+    </div>
+</template>
+
+<script>
+    import axios from 'axios';
+
+    export default {
+        name: "RegistrationForm",
+        methods: {
+            formSubmit() {
+                axios.post('/api/v1/user', {
+                        lolId: this.lolId,
+                    },
+                ).then(function (response) {
+                    window.location = "/";
+                }).catch(function (e) {
+                    window.location = "/error"
+                })
+            },
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
