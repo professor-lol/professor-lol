dacb955891c46a9844c65c33e70be064fa7fdc43
mjung1798 <mor2222@naver.com>
2019-08-31 23:22:29 +0900

    vue js custom complete

--- professorlol-web/src/front/src/App.vue
+++ professorlol-web/src/front/src/App.vue
@@ -1,13 +1,22 @@
 <template>
-  <router-view></router-view>
+  <v-app>
+    <core-app-bar />
+
+    <core-drawer />
+
+    <core-view />
+
+    <core-footer />
+  </v-app>
 </template>
 
 <script>
-export default {
-  name: 'app',
-
-}
+  export default {
+    components: {
+      CoreDrawer: () => import('@/components/core/Drawer'),
+      CoreFooter: () => import('@/components/core/Footer'),
+      CoreAppBar: () => import('@/components/core/AppBar'),
+      CoreView: () => import('@/components/core/View')
+    }
+  }
 </script>
-
-<style>
-</style>
